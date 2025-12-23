d6d9465e47c208f34c9023135797425c592251ff
gotoheaven1 <ansoyeolansoyeol@gmail.com>
2025-12-23 17:30:16 +0900

    Add files via upload

---
+++ tylahomepage/src/data/tylaData.js
@@ -0,0 +1,28 @@
+// 타일라 추천곡 리스트
+export const SONGS = [
+    { title: "Water", album: "Tyla" },
+    { title: "Truth or Dare", album: "Tyla" },
+    { title: "Getting Late", album: "Single" },
+    { title: "Been Thinking", album: "Single" },
+    { title: "On and On", album: "Tyla" }
+];
+
+// 수상 내역
+export const AWARDS = [
+    "2024 Grammy Awards - Best African Music Performance (Water)",
+    "2023 Soul Train Music Awards - Best New Artist (Nominated)",
+    "2023 MOBO Awards - Best African Music Act (Nominated)"
+];
+
+// 유튜브 영상 ID (예: https://www.youtube.com/watch?v=VIDEO_ID)
+export const YOUTUBE_IDS = [
+    "XoiOOiuH8iI", // Water MV
+    "hZqJk1qFjJA"  // Truth or Dare MV
+];
+
+// 소셜 링크
+export const SOCIAL_LINKS = [
+    { name: "YouTube", url: "https://www.youtube.com/@Tylamusic" },
+    { name: "Twitter (X)", url: "https://twitter.com/Tyllaaaaaaa" },
+    { name: "Instagram", url: "https://www.instagram.com/tyla" }
+];
